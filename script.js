fa6429ab3b8e17c1e4b470852bcb2eec7f5517d2
Henry Clyde <22669531+henryclyde@users.noreply.github.com>
2024-08-16 03:46:15 -0600

    table functions complete. Validation needed next

--- script.js
+++ script.js
@@ -8,42 +8,51 @@ function Book(title,author,readYet) {
     this.readYet = readYet;
 }
 
+
 function addBook() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
-    let readYet = document.getElementById("read");
+    let readYet = document.getElementById("read").checked;
     const newBook = new Book(title,author,readYet);
     myLibrary.push(newBook);
     let titleText = document.createTextNode(`${myLibrary[myLibrary.length-1].title}`);
     let authorText = document.createTextNode(`${myLibrary[myLibrary.length-1].author}`);
-    let readText =  document.createTextNode(`${readYet.checked === true ? "Yes" : "No"}`);
-    addBookToTable(titleText,authorText,readText);
-}
+    let readText =  document.createElement("input")
+    readText.type = "checkbox";
+    readText.checked = readYet.checked;
+    readText.addEventListener("click",()=>{
+        newBook.readYet = !newBook.readYet;
+    });
 
-function addBookToTable(t,a,r) {
     let table = document.querySelector("table");
 
     let newRow = document.createElement("tr");
-    let title = document.createElement("td");
-    let author = document.createElement("td");
-    let readYet = document.createElement("td");
+    let titleTd = document.createElement("td");
+    let authorTd = document.createElement("td");
+    let readYetTd = document.createElement("td");
+    let removeTd = document.createElement("td");
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+        removeButton.closest("tr").remove();
+    });
 
-    title.appendChild(t);
-    author.appendChild(a);
-    readYet.appendChild(r);
+    titleTd.appendChild(titleText);
+    authorTd.appendChild(authorText);
+    readYetTd.appendChild(readText);
+    removeTd.appendChild(removeButton);
 
-    newRow.appendChild(title);
-    newRow.appendChild(author);
-    newRow.appendChild(readYet);
+    newRow.appendChild(titleTd);
+    newRow.appendChild(authorTd);
+    newRow.appendChild(readYetTd);
+    newRow.appendChild(removeTd);
 
     table.appendChild(newRow);
+    document.getElementById("report").addEventListener("click", ()=>{
+        console.log(myLibrary);
+    });
 }
 
-function populateBooks() {
-    for (let i in myLibrary) {
-        let titleText = document.createTextNode(`${myLibrary[i].title}`);
-        let authorText = document.createTextNode(`${myLibrary[i].author}`);
-        let readText = document.createTextNode(`${myLibrary[i].readYet}`);
-        addBookToTable(titleText,authorText,readText);
-    }
-}
+
+
+
